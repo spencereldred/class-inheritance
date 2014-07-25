79b0faf859ab7a5d8c52db3fd13d739a84204fd6
Spencer Eldred <eldredspencer@gmail.com>
2014-07-25 08:51:11 -0600

    Square class inherits from Rectangle class

--- lib/square.rb
+++ lib/square.rb
@@ -1,16 +1,9 @@
-class Square
+class Square < Rectangle
   attr_accessor :side
 
   def initialize(side)
     @side = side
-  end
-
-  def area
-    @side**2
-  end
-
-  def perimeter
-    4*@side
+    super(side, side)
   end
 
 end
