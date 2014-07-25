7441cceaaec1c50aced7d11a45c245b9dfeb5c51
Spencer Eldred <eldredspencer@gmail.com>
2014-07-25 10:13:23 -0600

    Add README, delete contents of classes and tests

--- lib/square.rb
+++
@@ -1,9 +0,0 @@
-class Square < Rectangle
-  attr_accessor :side
-
-  def initialize(side)
-    @side = side
-    super(side, side)
-  end
-
-end
--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -1,24 +1,2 @@
 require 'spec_helper'
 
-describe Rectangle do
-
-  before do
-    @rectangle = Rectangle.new(4,5)
-  end
-
-  it "should have a length and a width" do
-    rectangle = Rectangle.new(4,5)
-    rectangle.width.should eq(5)
-    rectangle.length.should eq(4)
-  end
-
-  it "should have an area" do
-    rectangle = Rectangle.new(4,5)
-    rectangle.area.should eq(20)
-  end
-
-  it "should have a perimeter" do
-    rectangle = Rectangle.new(4,5)
-    rectangle.perimeter.should eq(18)
-  end
-end
