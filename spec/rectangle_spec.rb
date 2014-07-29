de108dcf18141fa892db795952267ef4a32a6c5d
Spencer Eldred <eldredspencer@gmail.com>
2014-07-29 15:39:59 -0600

    Add test for rectangle_count class variable and class method

--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -6,16 +6,27 @@ describe Rectangle do
     @rectangle = Rectangle.new(4,5)
   end
 
+  after do
+    @@rectangle_count = 0
+  end
+
   it "should have a length and a width" do
-    rectangle.width.should eq(5)
-    rectangle.length.should eq(4)
+    @rectangle.width.should eq(5)
+    @rectangle.length.should eq(4)
   end
 
   it "should have an area" do
-    rectangle.area.should eq(20)
+    @rectangle.area.should eq(20)
   end
 
   it "should have a perimeter" do
-    rectangle.perimeter.should eq(18)
+    @rectangle.perimeter.should eq(18)
+  end
+  it "should have a rectangle count" do
+    Rectangle.rectangle_count.should eq(1)
+    5.times do
+      Rectangle.new(3,4)
+    end
+    Rectangle.rectangle_count.should eq(6)
   end
 end
--- spec/square_spec.rb
+++ spec/square_spec.rb
@@ -2,18 +2,19 @@ require 'spec_helper'
 
 describe Square do
 
+  before do
+    @square = Square.new(5)
+  end
+
   it "should have a side" do
-    square = Square.new(5)
-    square.side.should eq(5)
+    @square.side.should eq(5)
   end
 
   it "should have an area" do
-    square = Square.new(5)
-    square.area.should eq(25)
+    @square.area.should eq(25)
   end
 
   it "should have a perimeter" do
-    square = Square.new(5)
-    square.perimeter.should eq(20)
+    @square.perimeter.should eq(20)
   end
 end
